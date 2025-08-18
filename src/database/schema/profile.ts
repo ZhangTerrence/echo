1f95d49b18c5bbef76fc8cc000320b892dc89fbb
ZhangTerrence <terrencezhang77@gmail.com>
2025-08-18 14:47:10 -0400

    feat: supabase integration

---
+++ src/database/schema/profile.ts
@@ -0,0 +1,10 @@
+import { pgTable, serial, text, uuid } from "drizzle-orm/pg-core";
+import { authUsers } from "drizzle-orm/supabase";
+
+export const profiles = pgTable("profiles", {
+  id: serial("id").primaryKey(),
+  username: text("username"),
+  user_id: uuid("user_id")
+    .references(() => authUsers.id)
+    .notNull(),
+});
